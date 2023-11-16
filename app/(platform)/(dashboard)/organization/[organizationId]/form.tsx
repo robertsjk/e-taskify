e0676af785c32d412a62ece901cea3a8d12ff7b1
codeduchy <codeduchy@gmail.com>
2023-11-16 17:11:08 +0000

    form components

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { createBoard } from "@/actions/create-board";
+import { FormInput } from "@/components/form/form-input";
+import FormSubmit from "@/components/form/form-submit";
 import { useAction } from "@/hooks/use-action";
 
 const Form = () => {
@@ -14,14 +16,17 @@ const Form = () => {
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
+    console.log(title);
 
     execute({ title });
   };
 
   return (
     <form action={onSubmit}>
-      <input type="text" placeholder="Enter" name="title" required />
-      <button>Submit</button>
+      <div className="flex flex-col space-y-2">
+        <FormInput id="title" errors={fieldErrors} label="Board Title" />
+      </div>
+      <FormSubmit variant="primary">Save</FormSubmit>
     </form>
   );
 };
