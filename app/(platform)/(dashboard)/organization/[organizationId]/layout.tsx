c770aeb57583d6589c7dd2b53c0699fb1e9c5c3c
codeduchy <codeduchy@gmail.com>
2023-11-16 12:19:22 +0000

    sidebar + settings

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,13 @@
+import { redirect } from "next/navigation";
+import OrgControl from "./_components/orgControl";
+
+const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
+};
+
+export default OrganizationIdLayout;
