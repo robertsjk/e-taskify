82b916edfc9152d6923c482c60141c2f74ccf5ad
codeduchy <codeduchy@gmail.com>
2023-11-17 12:22:11 +0000

    board popover form

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,15 @@
-import { db } from "@/lib/db";
-import Form from "./form";
+import { Separator } from "@/components/ui/separator";
+import Info from "./_components/info";
+import BoardList from "./_components/board-list";
 
 const OrganizationIdPage = () => {
   return (
-    <div>
-      <Form />
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 };
